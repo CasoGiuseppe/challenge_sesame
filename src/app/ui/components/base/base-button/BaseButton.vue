a51bda5ec7d4e1c03527f28e6d1d8e94b24bfd7a
Giuseppe CASO <giuseppecaso@MacBook-Air-de-ServiceDesk.local>
2024-06-12 11:21:37 +0200

    feature: add aria properties and custom classes

--- src/app/ui/components/base/base-button/BaseButton.vue
+++ src/app/ui/components/base/base-button/BaseButton.vue
@@ -1,11 +1,21 @@
 <template>
-  <component :is="is"></component>
+  <component
+    :is="is"
+    :disabled="disabled"
+    :aria-disabled="disabled"
+    :aria-invalid="disabled"
+    :aria-label="ariaLabel"
+    :class="[
+      'base-button',
+      `base-button--is-${type}`
+    ]"
+  ></component>
 </template>
 <script setup lang="ts">
-import type { UniqueId } from '@/app/ui/types';
-import type { PropType } from 'vue';
-import { ComponentIs } from './types';
-import { ensureValueCollectionExists } from '@/app/ui/validators/useCustomValidator';
+import type { UniqueId } from '@/app/ui/types'
+import type { PropType } from 'vue'
+import { ComponentIs, Types } from './types'
+import { ensureValueCollectionExists } from '@/app/ui/validators/useCustomValidator'
 
 defineProps({
   /**
@@ -22,7 +32,33 @@ defineProps({
   is: {
     type: String as PropType<ComponentIs>,
     default: ComponentIs.BUTTON,
-    validator: (prop: ComponentIs) => ensureValueCollectionExists({ collection: ComponentIs, value: prop})
+    validator: (prop: ComponentIs) =>
+      ensureValueCollectionExists({ collection: ComponentIs, value: prop })
+  },
+
+  /**
+   * Set the button type family [primary, secondary]
+   */
+  type: {
+    type: String as PropType<Types>,
+    default: Types.PRIMARY,
+    validator: (prop: Types) => ensureValueCollectionExists({ collection: Types, value: prop })
+  },
+
+  /**
+   * Set the disabled button state
+   */
+   disabled: {
+    type: Boolean as PropType<boolean>,
+    default: false
+  },
+
+  /**
+   * Set the aria accesibility label
+   */
+   ariaLabel: {
+    type: String as PropType<string>,
+    default: 'component aria label'
   },
 })
 </script>
