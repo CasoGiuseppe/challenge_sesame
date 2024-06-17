c61069b9a70b4ec8174145001be682a1278100f3
Giuseppe CASO <giuseppecaso@MacBook-Air-de-ServiceDesk.local>
2024-06-17 12:46:23 +0200

    refactor: update IconRate code

--- src/app/ui/components/elements/icons/IconRate.vue
+++ src/app/ui/components/elements/icons/IconRate.vue
@@ -1,11 +1,4 @@
 <template>
-  <!-- <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <g>
-      <path
-        d="M12,8.89L12.94,12h2.82l-2.27,1.62l0.93,3.01L12,14.79l-2.42,1.84l0.93-3.01L8.24,12h2.82L12,8.89 M12,2l-2.42,8H2 l6.17,4.41L5.83,22L12,17.31L18.18,22l-2.35-7.59L22,10h-7.58L12,2L12,2z"
-      />
-    </g>
-  </svg> -->
   <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M8.00421 11.1208L4.29581 13L5.00421 9.01985L2 6.20137L6.1458 5.62227L8 2L9.8542 5.62111L14 6.20021L10.9958 9.01869L11.7042 12.9988L8.00421 11.1208Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
