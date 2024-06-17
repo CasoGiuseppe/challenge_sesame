4a8184b15e8e5e867031945bfecf3c2ca936de1d
Giuseppe CASO <giuseppecaso@MacBook-Air-de-ServiceDesk.local>
2024-06-17 17:09:13 +0200

    refactor: restore App.vue state

--- src/app/ui/App.vue
+++ src/app/ui/App.vue
@@ -1,29 +1,9 @@
-<script setup lang="ts">
-import BaseButton from './components/base/base-button/BaseButton.vue';
-import BaseIcon from './components/base/base-icon/BaseIcon.vue';
-import LoadingState from './components/states/default/loading-state/LoadingState.vue'
-</script>
-
 <template>
   <Suspense>
-  <!-- <Suspense>
     <section class="app">
       <RouterView v-slot="{ Component }">
         <component :is="Component" />
       </RouterView>
     </section>
-  </Suspense> -->
-  <div>
-    <LoadingState></LoadingState>
-    <BaseIcon name="IconSearch" />
-  </div>
-</Suspense>
+  </Suspense>
 </template>
-<style scoped lang="scss">
-.app {
-  align-items: center;
-  display: flex;
-  justify-content: center;
-  min-height: 100vh;
-}
-</style>
