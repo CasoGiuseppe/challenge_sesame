f815d6b44ed65d8ec4ccb6c17c85c42e92594189
Giuseppe CASO <giuseppecaso@MacBook-Air-de-ServiceDesk.local>
2024-06-18 11:29:25 +0200

    feature: add props to define ID and component type

--- src/app/ui/components/elements/drawer-panel/DrawerPanel.vue
+++ src/app/ui/components/elements/drawer-panel/DrawerPanel.vue
@@ -1,6 +1,23 @@
 <template>
-    <dialog>
-        drawer
-    </dialog>
+  <dialog>drawer</dialog>
 </template>
+<script setup lang="ts">
+import type { UniqueId } from '@app/ui/types';
+import { type PropType } from 'vue';
+import { Types } from './types';
+import { ensureValueCollectionExists } from '@app/ui/validators/useCustomValidator';
+
+defineProps({
+  id: {
+    type: String as PropType<UniqueId>,
+    default: 'drawerPanel'
+  },
+
+  is: {
+    type: String as PropType<Types>,
+    default: Types.DRAWER,
+    validator: (prop: Types) => ensureValueCollectionExists({ collection: Types, value: prop })
+  }
+});
+</script>
 <style src="./DrawerPanel.scss" lang="scss"></style>
---
+++ src/app/ui/components/elements/drawer-panel/types.ts
@@ -0,0 +1,3 @@
+export enum Types {
+    DRAWER = 'drawer',
+}
