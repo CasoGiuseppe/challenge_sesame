1e541f5a11cea7f5d2c886d71c8f2ea150c57734
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-06-20 15:02:04 +0200

    feature: add first instance of menu with translations

--- src/app/ui/layouts/partials/main-navigation/MainNavigation.vue
+++ src/app/ui/layouts/partials/main-navigation/MainNavigation.vue
@@ -2,11 +2,36 @@
     <section class="main-navigation">
         <ResponsivePanel id="navigationPanel">
             <template #header>header</template>
-            <template #body>body</template>
+            <template #body>
+                <AccordionInfo open>
+                    <template #summary>
+                        {{ translate({key: 'MENU.role.admin' }) }}
+                    </template>
+                    <template #content>
+                        <AccordionInfo open customColor="6961ff">
+                            <template #summary>
+                                <BaseIcon name="IconRate" :size="Sizes.M" />
+                                {{ translate({key: 'MENU.sections.name' }) }}
+                            </template>
+                            <template #content>
+                                <BaseItemMenu>item menu</BaseItemMenu>
+                            </template>
+                        </AccordionInfo>
+                    </template>
+                </AccordionInfo>
+            </template>
         </ResponsivePanel>
     </section>
 </template>
 <script setup lang="ts">
 import ResponsivePanel from "@app/ui/components/tools/responsive-panel/ResponsivePanel.vue"
+import AccordionInfo from "@app/ui/components/elements/accordion-info/AccordionInfo.vue"
+import BaseIcon from "@app/ui/components/base/base-icon/BaseIcon.vue"
+import BaseItemMenu from "@app/ui/components/base/base-item-menu/BaseItemMenu.vue"
+import { Sizes } from "@app/ui/components/base/base-icon//types"
+
+import useTranslation from '@app/shared/composables/useTranslation';
+const { translate } = useTranslation();
+
 </script>
 <style src="./MainNavigation.scss" lang="scss"></style>
