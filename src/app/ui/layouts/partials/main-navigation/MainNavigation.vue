d1dc0d8d947fc0260c945be0285faafd5b92ffb3
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-06-20 14:38:27 +0200

    feature: add responsivePanel tool to mainNavigation section

--- src/app/ui/layouts/partials/main-navigation/MainNavigation.vue
+++ src/app/ui/layouts/partials/main-navigation/MainNavigation.vue
@@ -1,5 +1,12 @@
 <template>
-    <section style="background: red;">
-        main navigation
+    <section class="main-navigation">
+        <ResponsivePanel id="navigationPanel">
+            <template #header>header</template>
+            <template #body>body</template>
+        </ResponsivePanel>
     </section>
 </template>
+<script setup lang="ts">
+import ResponsivePanel from "@app/ui/components/tools/responsive-panel/ResponsivePanel.vue"
+</script>
+<style src="./MainNavigation.scss" lang="scss"></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -107,7 +107,7 @@ export default {
     transitionProperty: {
       colors:
         'color, background-color, border-color, text-decoration-color, fill, stroke, outline, box-shadow',
-      transform: 'transform, opacity top',
+      transform: 'transform, opacity, top',
       size: 'width, min-width, height, min-height',
       spacing: 'margin, padding, gap',
       opacity: 'opacity',
