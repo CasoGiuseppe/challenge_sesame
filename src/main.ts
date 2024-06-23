0a2ca38f0ff98bb98e717d58b470b5830024085e
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-06-23 17:13:16 +0200

    fix: remove unused provided

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,11 @@ import pinia from '@app/shared/stores/pinia';
 import { i18n } from '@app/translation';
 import ClickOutside from "@app/ui/directive/clickOutside";
 import useAsyncComponent from '@app/shared/composables/useAsyncComponent';
-import useRouterUtilities from '@app/shared/composables/useRouterUtilities';
-import useTranslation from '@app/shared/composables/useTranslation';
 import type { IAsyncComponent } from '@app/shared/composables/interfaces/IAsyncComponent';
 
 import "@assets/index.scss";
 import {
-  keyUseTranslations,
   keyUseAsyncComponent,
-  keyUseRouterUtilities
 } from "@app/shared/types/symbols";
 
 // composable instances
@@ -22,10 +18,11 @@ const { create } = useAsyncComponent();
 const app = createApp(defineAsyncComponent(() => import('@app/ui/layouts/App.vue')));
 
 app
-  .directive('click-outside', ClickOutside)
-  .use(router)
-  .use(i18n)
-  .use(pinia);
+.directive('click-outside', ClickOutside)
+.use(router)
+.use(i18n)
+.use(pinia);
+
 
 router.isReady().then(() => {
   app.mount('#app')
