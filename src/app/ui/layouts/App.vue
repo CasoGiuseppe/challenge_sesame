6788f5e17359dd7616a84a4a953bbc2755275ed7
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-06-27 22:04:06 +0200

    feature: restore statusId

--- src/app/ui/layouts/App.vue
+++ src/app/ui/layouts/App.vue
@@ -17,11 +17,11 @@ onMounted(async () => {
 
   //vacancy.getVacancyByID('e5d90a95-ec3f-4a15-b884-bbea519f1e05')
   await applicants.createApplicant({
-    firstName: 'ciccio', lastName: 'pasticcio', vacancyId: 'e5d90a95-ec3f-4a15-b884-bbea519f1e05', statusId: '1'
+    firstName: 'ciccio', lastName: 'pasticcio', vacancyId: 'e5d90a95-ec3f-4a15-b884-bbea519f1e05', statusId: '397627b9-7856-47fc-a918-f2055108d0a0'
   })
   // await applicants.createApplicant({
   //   firstName: 'xxx', lastName: 'yyy', vacancyId: 'e5d90a95-ec3f-4a15-b884-bbea519f1e05', statusId: '397627b9-7856-47fc-a918-f2055108d0a0'
   // })
-  // await applicants.getApplicantsByID('e5d90a95-ec3f-4a15-b884-bbea519f1e05')
+  await applicants.getApplicantsByID('e5d90a95-ec3f-4a15-b884-bbea519f1e05', )
 })
 </script>
