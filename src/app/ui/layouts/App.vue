44ce180b7f650867ff44dde0b322bbf47f07e692
Giuseppe CASO <giuseppecaso@MacBook-Air-de-ServiceDesk.local>
2024-06-27 14:38:20 +0200

    feature: add appliants dependencies

--- src/app/ui/layouts/App.vue
+++ src/app/ui/layouts/App.vue
@@ -12,7 +12,10 @@ import { dependencies } from '@/modules/core/dependencies';
 import { onMounted } from 'vue';
 
 onMounted(() => {
-  const ploc = dependencies.provideVacancyPloc()
-  ploc.getVacancyByID('e5d90a95-ec3f-4a15-b884-bbea519f1e05')
+  const vacancy = dependencies.provideVacancyPloc()
+  const applicants = dependencies.provideApplicantPloc()
+
+  vacancy.getVacancyByID('e5d90a95-ec3f-4a15-b884-bbea519f1e05')
+  applicants.getApplicationByID('e5d90a95-ec3f-4a15-b884-bbea519f1e05')
 })
 </script>
--- src/modules/core/dependencies/index.ts
+++ src/modules/core/dependencies/index.ts
@@ -1,7 +1,10 @@
 import { useRouter } from "vue-router"
 import { VacancyStateRepository } from "@modules/recruitment/modules/positions/data/repository/VacancyStateRepository"
 import { GetVacancyByIdUseCase } from "@modules/recruitment/modules/positions/domain/application/use-cases/GetVacancyByIdUseCase"
+import { ApplicantRepository } from "@modules/recruitment/modules/applicants/data/repository/ApplicantRepository"
 import { VacancyBloc } from "@modules/recruitment/modules/positions/presentation/bloc"
+import { ApplicantBloc } from "@modules/recruitment/modules/applicants/presentation/bloc"
+import { GetApplicantsByVacancyIdUseCase } from "@modules/recruitment/modules/applicants/domain/application/use-cases/GetApplicantsByVacancyId"
 import { HTTPServiceProvider } from "../providers/http/http.provide"
 
 const provideVacancyPloc = () => {
@@ -16,4 +19,19 @@ const provideVacancyPloc = () => {
     })
 }
 
-export const dependencies = { provideVacancyPloc }
+const provideApplicantPloc = () => {
+    const router = useRouter()
+
+    const applicantRepository = new ApplicantRepository(new HTTPServiceProvider())
+    const getApplicantsByVacancyId = new GetApplicantsByVacancyIdUseCase(applicantRepository)
+
+    return new ApplicantBloc({
+        router,
+        getApplicantsByVacancyId
+    })
+}
+
+export const dependencies = {
+    provideVacancyPloc,
+    provideApplicantPloc
+}
