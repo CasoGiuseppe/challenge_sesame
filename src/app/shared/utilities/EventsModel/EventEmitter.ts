416a2068d7b49a8ae985f7e456d60e0348610a20
Giuseppe CASO <giuseppecaso@macbook-air-de-servicedesk.home>
2024-07-02 09:35:57 +0200

    feature: add emitter to handle notification

---
+++ src/app/shared/utilities/EventsModel/EventEmitter.ts
@@ -0,0 +1,30 @@
+import type { EventCallback } from "./types";
+
+export class EventEmitter {
+    private events: Record<string, EventCallback[]>;
+    constructor() {
+      this.events = {};
+    }
+    public subscribe(eventName: string, callback: EventCallback): void {
+      if (!this.events[eventName]) {
+        this.events[eventName] = [];
+      }
+      this.events[eventName].push(callback);
+    }
+    public unsubscribe(eventName: string, callback: EventCallback): void {
+      const eventCallbacks = this.events[eventName];
+      if (eventCallbacks) {
+        this.events[eventName] = eventCallbacks.filter(
+          (cb) => cb !== callback
+        );
+      }
+    }
+    public emit(eventName: string, ...args: any[]): void {
+      const eventCallbacks = this.events[eventName];
+      if (eventCallbacks) {
+        eventCallbacks.forEach((callback) => {
+          callback(...args);
+        });
+      }
+    }
+  }
